3ad14dd0f58fb6a918f4d9791abb4baedbbb2810
DucHuySET <huy.nd203450@sis.hust.edu.vn>
2023-06-10 02:29:46 +0700

    remove wrong position file

--- OCR/core/values/strings.py
+++
@@ -1,6 +0,0 @@
-class AppStr:
-    NO_IMAGE_DETECT = 'Lỗi: không lấy được ảnh'
-    DETECT_SUCCESS = 'Nhận diện thành công'
-    DETECT_FAILED = 'Nhận diện không thành công'
-    PLEASE_WAIT = 'Xin vui lòng chờ giây lát'
-    CANT_READ_SCALE = 'Không đọc được giá trị cân'
